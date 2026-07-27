e157e0bfb4836cb1d3846738dd2fc23422991431
Henrique <hebaja@yahoo.com.br>
2026-07-27 00:50:22 -0300

    add wordpress init file

---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+DB_PASS=$(cat /run/secrets/db_password)
+WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASS=$(cat /run/secrets/wp_user_password)
+
+until mysqladmin ping -h "mariadb" -u " $DB_USER " -p " $DB_PASS" --silent; do
+	sleep 2
+done
+
+if ! wp core is-installed --allow-root 2>/dev/null; then
+	wp config create \
+		--dbname="$DB_NAME" \
+		--dbuser="$DB_USER" \
+		--dbpass="$DB_PASS" \
+		--dbhost="mariadb" \
+		--allow-root
+
+	wp core install \
+		--url="$DOMAIN_NAME" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN" \
+		--admin_password="$WP_ADMIN_PASS" \
+		--admin_email="$WP_ADMIN_EMAIL" \
+		--allow-root
+
+	wp user create "$WP_USER" "$WP_USER_EMAIL" \
+		--role=subscriber \
+		--user_pass="$WP_USER_PASS" \
+		--allow-root
+fi
+
+exec php-fpm82 -F
