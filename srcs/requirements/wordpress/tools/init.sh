9b3e44b645414ee5cc634c72a009fe8a81a5a43b
Henrique <hebaja@yahoo.com.br>
2026-07-27 23:55:17 -0300

    wordpress docker-compose section fixed

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -21,7 +21,7 @@ if ! wp core is-installed --allow-root 2>/dev/null; then
 	wp core install \
 		--url="$DOMAIN_NAME" \
 		--title="$WP_TITLE" \
-		--admin_user="$WP_ADMIN" \
+		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$WP_ADMIN_PASS" \
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--allow-root
