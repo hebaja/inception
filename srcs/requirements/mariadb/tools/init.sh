72421c8c4a0480590e05a3f7b5c4b827cdfcea5f
hebatist <hebaja@yahoo.com.br>
2026-07-28 23:22:57 -0300

    fix mariadb init.sh

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -15,9 +15,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
 	mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat /run/secrets/db_root_password)';
-CREATE DATABASE IF NOT EXISTS $(cat /run/secrets/db_name);
-CREATE USER IF NOT EXISTS '$(cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
-GRANT ALL PRIVILEGES ON $(cat /run/secrets/db_name).* TO '$(cat /run/secrets/db_user)'@'%';
+	CREATE DATABASE IF NOT EXISTS $DB_NAME;
+	CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
+	GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
