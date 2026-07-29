da1db5d7bc6110652d3c1767b2bfae04d47e1966
hebatist <hebaja@yahoo.com.br>
2026-07-28 22:07:51 -0300

    optimize mariadb settings

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,16 +4,16 @@ mkdir -p /run/mysqld
 chown mysql:mysql /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-    /usr/bin/mariadbd --user=mysql --skip-networking &
-    pid=$!
+	/usr/bin/mariadbd --user=mysql --skip-networking &
+	pid=$!
 
 	until mysqladmin ping --silent; do
 		sleep 1
 	done
 
-    mysql -u root <<EOF
+	mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat /run/secrets/db_root_password)';
 CREATE DATABASE IF NOT EXISTS $(cat /run/secrets/db_name);
 CREATE USER IF NOT EXISTS '$(cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
@@ -21,8 +21,8 @@ GRANT ALL PRIVILEGES ON $(cat /run/secrets/db_name).* TO '$(cat /run/secrets/db_
 FLUSH PRIVILEGES;
 EOF
 
-    mysqladmin -u root -p"$(cat /run/secrets/db_root_password)" shutdown
-    wait $pid
+	mysqladmin -u root -p"$(cat /run/secrets/db_root_password)" shutdown
+	wait $pid
 fi
 
-exec /usr/bin/mariadbd --user=mysql
+exec "$@"
